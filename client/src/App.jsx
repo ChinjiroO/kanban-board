2e8a8edcf84a3b275fbfdd6e48aa0625b39a929d
ChinjiroO <techinseoikrathok@gmail.com>
2022-04-15 00:49:37 +0700

    add: board component in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,22 +2,23 @@ import React, { useState } from "react";
 
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import Board from "./components/Board";
 
 function App() {
-	const [open, setOpen] = useState(false);
-	const handleOpen = () => {
-		setOpen(!open);
-	};
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(!open);
+  };
 
-	return (
-		<div className="h-screen overflow-hidden">
-			<Navbar onClick={handleOpen} open={open} />
-			<main className="flex">
-				{open ? <Sidebar /> : null}
-				<div className="">This's body</div>
-			</main>
-		</div>
-	);
+  return (
+    <div className="h-screen overflow-hidden">
+      <Navbar onClick={handleOpen} open={open} />
+      <main className="flex bg-white h-full">
+        {open ? <Sidebar /> : null}
+        <Board />
+      </main>
+    </div>
+  );
 }
 
 export default App;
