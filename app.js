9c33e683774ece8be77ba1d7e247c1e34c422f7b
ChinjiroO <techinseoikrathok@gmail.com>
2022-05-04 07:17:23 +0700

    update: setting up Express.js

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
 
-const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
+const app = express()
 
-app.use(cors());
-app.listen(port, () => console.log(`listening on port ${port}`));
+// Middleware
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Node.js, Express, and Postgres API started' })
+})
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
