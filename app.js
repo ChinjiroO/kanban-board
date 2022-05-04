f9a70c1ccb660db8e013e69d5ce5b1f0f7dee3d0
ChinjiroO <techinseoikrathok@gmail.com>
2022-05-04 10:42:30 +0700

    chore: update linter and prettier format

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 
-const port = process.env.PORT || 5000
-const app = express()
+const port = process.env.PORT || 5000;
+const app = express();
 
 // Middleware
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Node.js, Express, and Postgres API started' })
-})
+  res.json({ message: 'Node.js, Express, and Postgres API started' });
+});
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}`)
-})
+  console.log(`listening on port ${port}`);
+});
