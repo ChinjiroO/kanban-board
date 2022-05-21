2c1b5ea32a5733bec5c324e94925eb2ec1a5d5cb
ChinjiroO <techinseoikrathok@gmail.com>
2022-05-22 02:58:39 +0700

    feat: add createUser function

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,6 +1,21 @@
+const bcrypt = require('bcrypt');
+
 const Users = require('../models').users;
 
 module.exports = {
+  async createUser(req, res) {
+    return Users.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: await bcrypt.hash(req.body.password, 10),
+    })
+      .then(() =>
+        res
+          .status(200)
+          .send({ message: `create user: ${req.body.username} successfully` })
+      )
+      .catch((err) => res.status(500).send({ message: err.message }));
+  },
   allUsers(req, res) {
     return Users.findAll({})
       .then((users) => res.status(200).send(users))
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 const userController = require('../controller/user.controller');
 
+// create a new user
+router.post('/signup', userController.createUser);
 // Get all users
 router.get('/', userController.allUsers);
 
