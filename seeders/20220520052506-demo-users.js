aec6fcabe989095f898c880f094e931153135bfc
ChinjiroO <techinseoikrathok@gmail.com>
2022-05-24 00:57:16 +0700

    update seeds

--- seeders/20220520052506-demo-users.js
+++ seeders/20220520052506-demo-users.js
@@ -1,19 +1,30 @@
+const bcrypt = require('bcrypt');
+
+const plainText = '1234';
+
 module.exports = {
   async up(queryInterface) {
     await queryInterface.bulkInsert(
       'users',
       [
         {
-          username: 'test_users_1',
-          email: 'test@example.com',
-          password: '12345678',
+          username: 'test01',
+          email: 'test_01@example.com',
+          password: bcrypt.hashSync(plainText, 8),
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
-          username: 'test_users_2',
-          email: 'test2@example.com',
-          password: '12345678',
+          username: 'test02',
+          email: 'test_02@example.com',
+          password: bcrypt.hashSync(plainText, 8),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          username: 'test03',
+          email: 'test_03@example.com',
+          password: bcrypt.hashSync(plainText, 8),
           createdAt: new Date(),
           updatedAt: new Date(),
         },
